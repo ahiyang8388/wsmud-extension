f178bcea7e7e1d40883ad7b888a23b739f440fc0
Mingy <zhengmao.w@gmail.com>
2018-01-18 23:31:51 +0800

    update

--- script/addon.js
+++ script/addon.js
@@ -1004,7 +1004,7 @@
 					SendCommand('eq 86q7155246a;eq cd9r156c5c0;eq 2qfb188cf4d;eq 40z51332c8f;eq sg9w14d7dca;eq x6e51518454');
 					e.preventDefault();
 				} else if (e.which == 118) { // F7
-					SendCommand('eq iq8b15a9c27;eq 1tgm18a2aaf;eq 2qfb188cf4d;eq 40z51332c8f;eq sg9w14d7dca;eq x6e51518454');
+					SendCommand('eq iq8b15a9c27;eq 1tgm18a2aaf;eq 2qfb188cf4d;eq 40z51332c8f;eq nc6v18cdca3;eq x6e51518454');
 					e.preventDefault();
 				} else if (e.which == 119) { // F8
 					SendCommand('eq 603z155852b;eq cd9r156c5c0;eq wxth16a8173;eq q0ui10f5a1d;eq lhc313bbbf4;eq buhp157ff22');
